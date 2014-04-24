400857446a59d463a61da46df08ec19021d4d70c
krzysiug <krzysiekg18@gmail.com>
2014-04-24 20:13:02 +0200

    dodany kontrakt dla ICanTransferMoney

---
+++ ICanTransferMoney.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface ICanTransferMoney
+    {
+        [OperationContract]
+        int TransferMoney(long accountNumber1, long accountNumber2, decimal value);
+    }
+}
