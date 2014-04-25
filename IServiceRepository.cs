61303c2746e87fafdb71ec60a571711e982bb158
Rafał Płonka <rufhs1@gmail.com>
2014-04-25 18:33:38 +0200

    dodany kontrakt IServiceRepository

---
+++ IServiceRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace Contracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    [ServiceContract]
+    public interface IServiceRepository
+    {
+        [OperationContract]
+        public void RegisterService(string name, string address);
+
+        [OperationContract]
+        public void UnregisterService(string name);
+
+        [OperationContract]
+        public string GetServiceLocation(string name);
+
+        [OperationContract]
+        public bool IsAlive(string name);
+    }
+
+    [DataContract]
+    public class AccountInfo
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+    }
+}
