932ddc6cb9f28ab390fd97aa0241b9000bc79fd7
szybkiekonto <wojciechzieba@gmail.com>
2014-04-27 21:32:21 +0200

    Create IClientRepository.cs

---
+++ IClientRepository.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface IClientRepository
+    {
+        [OperationContract]
+        long CreateClient(string name, string lastName, string PESEL, string address);
+
+        [OperationContract]
+        IEnumerable<Client> GetClientInformationById(Guid idClient);
+
+        [OperationContract]
+        IEnumerable<Client> GetClientInformationByName(string name, string lastName);
+
+        [OperationContract]
+        bool RemoveClientByName(string name, string lastName);
+
+        [OperationContract]
+        bool RemoveClientById(Guid idClient);
+    }
+    [DataContract]
+    public class Client
+    {
+        [DataMember]
+        public Guid IdClient { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Lastname { get; set; }
+        [DataMember]
+        public string PESEL { get; set; }
+        [DataMember]
+        public string address { get; set; }
+    }
+}
