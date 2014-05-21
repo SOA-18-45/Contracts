2d9063becadfd4fecdadf3768b169dcb8fe8d1d5
Haxer557 <przemek.szczepaniak@o2.pl>
2014-05-21 19:30:15 +0200

    dodano prostą wersję IAuditorService, żaden inny serwis chyba z tego nie korzysta więc nie musicie się tym przejmować :P

---
+++ Contracts/Contracts/IAuditorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+
+    [ServiceContract]
+    public interface IAudtitorService
+    {
+        [OperationContract]
+        int GetClientCount();
+        [OperationContract]
+        int GetAccountCount();
+        [OperationContract]
+        int GetRORCount();
+        [OperationContract]
+        IEnumerable<BasicAccountInfo> GetAccounts();
+        [OperationContract]
+        bool AuditAll();
+    }
+
+    [DataContract]
+    public class BasicAccountInfo
+    {
+        [DataMember]
+        string AccountNumber;
+        [DataMember]
+        decimal money;
+    }
+}
