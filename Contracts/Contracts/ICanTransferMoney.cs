b806e7a622fe3999891a4823155996b25642678c
Mariusz Pilarczyk <pilaas@o2.pl>
2014-05-22 15:26:14 +0200

    Dodanie klasy HistoryItem do kontraktu w ICanTransferMoney

--- Contracts/Contracts/ICanTransferMoney.cs
+++ Contracts/Contracts/ICanTransferMoney.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ServiceModel;
+using System.Runtime.Serialization;
 
 
 namespace Contracts
@@ -17,12 +18,27 @@ namespace Contracts
         int TransferMoneyGuid(Guid AccountGuid1, Guid AccountGuid2, double value); // pozwala na wykonanie przelewu na podstawie numeru Guid kont
 
         [OperationContract]
-        int TransferHistory(DateTime DateFrom, DateTime DateTo, String AccountNumber); // pozwala na zwrócenie historii przelewów dla danego numeru konta w podanych zakresie czasowym
+        List<HistoryItem> TransferHistory(DateTime DateFrom, DateTime DateTo, String AccountNumber); // pozwala na zwrócenie historii przelewów dla danego numeru konta w podanych zakresie czasowym
 
         [OperationContract]
-        int TransferHistoryAccountGuid(DateTime DateFrom, DateTime DateTo, Guid AccountGuid); // pozwala na zwrócenie historii przelewów dla danego numeru Guid konta w podanych zakresie czasowym
+        List<HistoryItem> TransferHistoryAccountGuid(DateTime DateFrom, DateTime DateTo, Guid AccountGuid); // pozwala na zwrócenie historii przelewów dla danego numeru Guid konta w podanych zakresie czasowym
         
         [OperationContract]
-        int TransferHistory(DateTime DateFrom, DateTime DateTo); // pozwala na zwrócenie historii przelewów dla WSZYSTKICH kont w podanych zakresie czasowym
+        List<HistoryItem> TransferHistory(DateTime DateFrom, DateTime DateTo); // pozwala na zwrócenie historii przelewów dla WSZYSTKICH kont w podanych zakresie czasowym
+    }
+    
+    [DataContract]
+    public class HistoryItem
+    {
+        [DataMember]
+        public Guid ID { get; set; }
+        [DataMember]
+        public string AccountFrom { get; set; }
+        [DataMember]
+        public string AccountTo { get; set; }
+        [DataMember]
+        public double Value { get; set; }
+        [DataMember]
+        public DateTime TransactionDate { get; set; }
     }
 }
