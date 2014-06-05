a08bc563e488dbe6e83067e1c85f69906334d11a
grymbol <grymbol@gmail.com>
2014-06-06 00:38:19 +0200

    Nowe pole BindingType (ważne!)
    Dodałem pole BindingType oraz zaktualizowałem metody, tak żeby z tego nowego pola korzystały. Najlepiej, żeby przyjmowało ono wartości tekstowe "net.tcp" lub "http" pisane małymi literami, tak żeby później nie było problemów z odczytem/zapisem.

--- Contracts/Contracts/IServiceRepository.cs
+++ Contracts/Contracts/IServiceRepository.cs
@@ -15,19 +15,22 @@ namespace Contracts
 
         [DataMember]
         public string ServiceAddress { get; set; }
+        
+        [DataMember]
+        public string BindingType { get; set; }
     }
 
     [ServiceContract]
     public interface IServiceRepository
     {
         [OperationContract]
-        void registerService(string serviceName, string serviceAddress);
+        void registerService(string serviceName, string serviceAddress, string bindingType);
 
         [OperationContract]
         void unregisterService(string serviceName);
 
         [OperationContract]
-        string getServiceAddress(string serviceName);
+        string getServiceAddress(string serviceName, string bindingType);
 
         [OperationContract]
         void isAlive(string serviceName);
